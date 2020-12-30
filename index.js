99c4218993a1e237498d7cb17abe0fac674cde59
ShimeX <62620132+ShimeXx@users.noreply.github.com>
2020-12-30 09:39:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const http = require('http');
 const express = require('express');
 const app = express();
 
 
 app.use(express.static('public'));
 
-http.createServer(app).listen(80); {
-    app.get('/', function(req, res) {
-        res.render('index.ejs');
-    });
-};
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+});
 
-const port = process.env.PORT|| 80;
-console.log(`server şu portta açık: ${port}`);
+const port = process.env.PORT|| 3000;
+app.listen(port, () => {console.log(`server şu portta açık: ${port}`)});
